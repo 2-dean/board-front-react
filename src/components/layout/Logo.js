d3af6fca093a8c4dabc99ea1f7dc665f0a78535f
ddu <limsjs@naver.com>
2025-02-24 17:17:42 +0900

    로고이미지 추가

--- src/components/layout/Logo.js
+++ src/components/layout/Logo.js
@@ -1,9 +1,10 @@
+import logo from "../../assets/logo.png";
 
 
 const Logo = ({ onClick }) => {  // ✅ 부모에서 전달한 onClick을 받음
     return (
         <div className="sidebar-header" onClick={onClick}>
-            <img src={""} alt="Logo" className="sidebar-logo"/>
+            <img src={logo} alt="Logo" className="sidebar-logo"/>
         </div>
     );
 };
